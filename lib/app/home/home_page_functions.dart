aa377fafe6e2b40e6b5ffd56925c643c2f222c51
TheWill01 <will962@live.com>
2021-12-03 16:53:09 -0300

    AtualizaçãoRequisições

---
+++ lib/app/home/home_page_functions.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:trabalhofinalbd2/app/globals/globals_functions.dart';
+import 'package:trabalhofinalbd2/app/globals/globals_var.dart';
+import 'package:trabalhofinalbd2/app/globals/globals_widgets.dart';
+import 'package:http/http.dart' as http;
+import 'package:trabalhofinalbd2/app/home/store/home_page_store.dart';
+
+class HomePageFunctions {
+  BuildContext context;
+  HomePageFunctions(this.context);
+
+  Future limpaBancoDeDados() async {
+    final homePageStore = Provider.of<HomePageStore>(context, listen: false);
+    homePageStore.setCarregandoPagina(true);
+
+    dynamic response;
+
+    if (!(await GlobalsFunctions(context).verificaInternet())) {
+      try {
+        response = await http.delete(
+          Uri.parse(
+            '${GloblasVars(context).urlEp}/moeda.php',
+          ),
+        );
+        if (response.body == '1') {
+          print('RESPONSEBODYDELETE>>> ${response.body}');
+          homePageStore.setCarregandoPagina(false);
+          /*GlobalsWidgets(context).alertSucesso(getCotacao);*/
+        } else {
+          GlobalsWidgets(context).alertErroEnvio();
+        }
+      } catch (e) {
+        print("ERRO GET COTACAO>> $e");
+        print("RESPONSE ERRO>> $response");
+        GlobalsWidgets(context).alertErroEnvio();
+      }
+    } else {
+      GlobalsWidgets(context).alertSemInternet();
+    }
+  }
+}
--- lib/app/home/store/home_page_store.g.dart
+++ lib/app/home/store/home_page_store.g.dart
@@ -9,10 +9,39 @@ part of 'home_page_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomePageStore on _HomePageStore, Store {
+  final _$carregandoPaginaAtom = Atom(name: '_HomePageStore.carregandoPagina');
+
+  @override
+  bool get carregandoPagina {
+    _$carregandoPaginaAtom.reportRead();
+    return super.carregandoPagina;
+  }
+
+  @override
+  set carregandoPagina(bool value) {
+    _$carregandoPaginaAtom.reportWrite(value, super.carregandoPagina, () {
+      super.carregandoPagina = value;
+    });
+  }
+
+  final _$_HomePageStoreActionController =
+      ActionController(name: '_HomePageStore');
+
+  @override
+  void setCarregandoPagina(dynamic _value) {
+    final _$actionInfo = _$_HomePageStoreActionController.startAction(
+        name: '_HomePageStore.setCarregandoPagina');
+    try {
+      return super.setCarregandoPagina(_value);
+    } finally {
+      _$_HomePageStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+carregandoPagina: ${carregandoPagina}
     ''';
   }
 }
